0da20627c9bcbc839a5c6888c393f71775da8496
Sai Sathwik <reddysaisathwik022@gmail.com>
2024-07-03 20:20:40 +0530

    env and appwrite setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+  return (
+    <>
+      <h1>Blog App with AppWrite</h1>
+    </>
+  );
+}
+
+export default App;
