b4c03317506d0ba127096b730183cdb390a49fe7
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-07-20 17:20:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store/store.js";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+create from scratch
