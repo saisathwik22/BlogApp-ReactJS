ad885b95505938a57784d7b692782a251d1ad8a1
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2024-07-20 17:18:35 +0530

    Delete src/components directory

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import React from "react";
-
-function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
-}
-
-export default Logo;
