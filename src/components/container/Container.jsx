3d82edc6d8c93826b337ac086ce4f5cf2b5f8757
saisathwik22 <reddysaisathwik022@gmail.com>
2024-07-27 13:03:22 +0530

    components button, input, logo, footer, header, logoutbtn, container
    CodeStage: 22

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Container({ children }) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+}
+
+export default Container;
